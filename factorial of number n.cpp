1cf5f619878cd485481a2136fbefb878827275e7
shahzaiba872 <shahzaibajutt872@gmail.com>
2025-02-18 19:32:01 +0500

    Add files via upload

---
+++ factorial of number n.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std ;
+int  main() {
+	int num ;	
+	cout<<"Enter a number :";
+	cin>>num ;
+	long long fact= facctorial (num) ;
+	
+	if(fact!=-1){
+	
+	
+			
+	
+	cout<< "factorial of " <<" num"<<"is"<<fact << endl ;
+}
+     
+
+	return 0 ;
+}
+

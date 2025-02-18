0459fd3de883d6c3cd2380d613a6d1ab99878c5a
shahzaiba872 <shahzaibajutt872@gmail.com>
2025-02-18 20:07:17 +0500

    Add files via upload

--- if else.cpp
+++ if else.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int year;
-	cout<<"enter a year";
-    cin>>year;
-if((year%4==0 && year%100!=0)||(year%400==0))	
-{
-cout<<year<<"is a leap year"<<endl;	}	
-else{cout<<year<<"year is not leap year"<<endl;
-
+# include <iostream>
+using namespace std ;
+int main (){
+	int num ;
+	cout<<" Enter a number :";
+	cin>> num ;
+	if ( num <0){
+		cout<<" The number is negative." endl;
+	} else {
+		cout<<"The number is positive ."<<endl;
+	}
+	return 0;
 }
-return 0;}
 
